46e23727a2e7609745681b07138967acd8188ec2
JuanMolfese <juanjosemolfese@gmail.com>
2024-02-14 23:52:02 -0300

    se implementar insert en la BBDD de rubro y sub rubros

--- app/utils/actions/rubros/create.tsx
+++ app/utils/actions/rubros/create.tsx
@@ -1,11 +1,15 @@
 "use server"
 
+import { connection } from "../../models/db";
+
 export default async function createRubro(formData: FormData) {
   try {
     const rawFormData = {            
       nombre: formData.get("name"),      
     };
-    console.log(rawFormData);
+    //Aqui hacer verificaciones antes de insertar en BBDD
+    await connection.query('INSERT INTO Rubro (nombre) VALUES (?)', [rawFormData.nombre])      
+    return ("OK")
   }
   catch (error) {
     console.log(error);
--- app/utils/actions/subrubros/create.tsx
+++ app/utils/actions/subrubros/create.tsx
@@ -1,5 +1,7 @@
 "use server"
 
+import { connection } from "../../models/db";
+
 export default async function createSubrubro(formData: FormData) {
   try {
     const rawFormData = {      
@@ -7,7 +9,9 @@ export default async function createSubrubro(formData: FormData) {
       nombre: formData.get("name"),
       
     };
-    console.log(rawFormData);
+    //Aqui hacer verificaciones antes de insertar en BBDD
+    await connection.query('INSERT INTO Subrubro (rubro_id, nombre) VALUES (?, ?)', [rawFormData.rubro_id, rawFormData.nombre])    
+    return ("OK")
   }
   catch (error) {
     console.log(error);
