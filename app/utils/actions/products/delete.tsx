5280a7b862893991860430435be7a508f4c206ce
fdcrespi <fdcrespi@gmail.com>
2024-03-10 10:12:52 -0300

    Se agrego la opción para eliminar producto

--- app/utils/actions/products/delete.tsx
+++ app/utils/actions/products/delete.tsx
@@ -1,24 +1,28 @@
 'use server'
 
 import { connection } from "../../models/db";
-import { ProductoDetail } from "../../models/types/producto";
+import { Producto, ProductoDetail } from "../../models/types/producto";
+import { Sucursal_productos } from "../../models/types/sucursal_productos";
 
 export default async function deleteProduct(id: number){
   try {
     //const result = await connection.query<Producto>("DELETE FROM Producto WHERE id = ?", [id]);
-    const result = await connection.query<ProductoDetail>(`
-      DELETE p, sp
-      FROM Producto p
-      JOIN Sucursal_Productos sp ON sp.producto_id = p.id
-      WHERE p.id = ?`, 
-      [id]
-    );
+    const resSP = await connection.query<Sucursal_productos>(`
+        DELETE FROM Sucursal_Productos WHERE producto_id = ?`,
+        [id]
+      );
+    if (resSP.producto_id === id) {
+      const resP = await connection.query<Producto>(`
+        DELETE FROM Producto WHERE id = ?`,[id]
+      );
+    }
     await connection.end();
     return { 
       success: true,
       message: 'Product Deleted.' 
     };
   }  catch (error) {
+    console.log(error);
     return { 
       success: false,
       message: 'Database Error: Failed to Delete Product.' 
