485d4318fef2d39402620cf5cf1c068fcf811a55
JuanMolfese <juanjosemolfese@gmail.com>
2024-05-11 21:04:26 -0300

    Update dependencies, add Embla Carousel and remove Header and Footer components from Home page
    * Upgrade Next.js to version 14.2.3
    * Add Embla Carousel, Embla Carousel

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,17 @@
 export default function Footer() {
     return(
-        <h1>Esto es el footer</h1>
+    <footer className="bg-accent-1 border-t border-accent-2 lg:w-2/3">      
+        <div className="py-20 flex flex-col lg:flex-row items-center">
+          <h3 className="text-4xl lg:text-6xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/3">
+            El Balcon
+          </h3>
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/3">
+            <p>Chacabuco 129</p>
+          </div>
+          <div className="lg:w-1/3 text-center">
+            <p>Tres Arroyos</p>            
+          </div>            
+        </div>      
+    </footer>
     )    
 };
---
+++ components/Hero/Slider.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image'
+import {images} from './images/image'
+import { Button } from '@/components/ui/button'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
+import React from 'react'
+
+const Slider = () => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 5000, stopOnInteraction: true })
+  )
+  return (
+    <div className="w-full min-h-screen mx-auto">
+      <Carousel plugins={[plugin.current]}
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}>
+
+        <CarouselContent>
+          {images.map((pic, index) => 
+          <CarouselItem key={index}>
+            <Image 
+                src={pic.src}
+                alt={pic.alt}
+                /* layout='fill' */
+                quality={100}
+                width={1080}
+                height={1920}
+                className='w-full h-full object-cover'
+                priority
+            />          
+          </CarouselItem>
+        )}
+        </CarouselContent>
+       {/*  <CarouselPrevious />
+        <CarouselNext /> */}
+      </Carousel>
+      <Button variant="destructive"
+        className='absolute bottom-3 left-5 animate-bounce rounded-full h-14'
+      >Ordenar ahora !</Button>
+    </div>
+  )}
+          
+{/*           'block w-full h-[80vh] object-cover transition-all duration-500 ease-in-out'
+          : "hidden" */}
+
+export default Slider
