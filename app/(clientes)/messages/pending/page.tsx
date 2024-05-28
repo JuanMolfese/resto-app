9bebeef733c4a146edfd88c6aa09fd066af94ef0
JuanMolfese <juanjosemolfese@gmail.com>
2024-05-28 20:10:27 -0300

    actualizacion de mensajes de retorno de la app luego de pasar por sandbox MP

--- app/(clientes)/messages/pending/page.tsx
+++ app/(clientes)/messages/pending/page.tsx
@@ -12,7 +12,7 @@ function pending() {
         <div className="flex justify-center">
           <button className="bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
             <Link href="/productos/">
-              Volver a El Balcon
+              Volver a Pizza 3 APP
             </Link>
           </button>
         </div>
--- app/(clientes)/messages/success/page.tsx
+++ app/(clientes)/messages/success/page.tsx
@@ -12,7 +12,7 @@ function success() {
         <div className="flex justify-center">
           <button className="bg-green-500 text-white px-6 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
             <Link href="/productos/">
-                Volver a El Balcon
+                Volver a Pizza 3 APP
             </Link>
           </button>
         </div>
