a584c1724b828994162c89b1aa79118af0f38afc
fedecrespi <fdcrespi@gmail.com>
2024-08-08 09:18:09 -0300

    Se modifico layout de dashboard para usar redux

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,19 +1,26 @@
+"use client";
 
+import { useGetProductsQuery } from "@/redux/services/productsApi";
 import MenuDashboard from "../../components/Dashboard/Layout/menu";
-import { fetchProductsOutofStock } from "../utils/actions/products/fetchs";
 import { getServerSession } from "next-auth";
-import { fetchUserByEmail } from "../utils/actions/users/fetchs";
+import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { useGetUserByEmailQuery } from "@/redux/services/usersApi";
+import Spinner from "../../components/spinner";
 
 
-export default async function Layout({children,}: 
+export default function Layout({children,}: 
   Readonly<{children: React.ReactNode;}>){
 
-  const productos = await fetchProductsOutofStock(1);
-  const session = await getServerSession();
+  const {data: session} = useSession();
+  const {data: productos, isLoading, error} = useGetProductsQuery();
+  const {data: userData, isLoading: userLoading, error: userError} = useGetUserByEmailQuery(session?.user?.email);
+  
+  if (userLoading || isLoading) return <Spinner />;
+  if (userError || error) return <div>Error</div>;
+
   if (session) {
     const email = session?.user?.email;
-    const userData = await fetchUserByEmail(email!);
     const user = userData && userData[0];
     user?.descripcion == 'Client' && redirect("/productos");
   } else {
